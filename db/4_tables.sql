b04fbc6a2c6a1e3ce58d44064d8461255b72a0f7
Heiko-Zelt <hz@heikozelt.de>
2020-09-13 21:20:26 +0200

    added CREATE TABLE SQL and Ansible script

---
+++ db/4_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.wegeheld_notices (
+    latitude double precision NOT NULL,
+    longitude double precision NOT NULL,
+    carbrand text,
+    date timestamp with time zone NOT NULL,
+    charge_id smallint,
+    fotolink text
+)
