232531f30f9a61a747a5d487b28cfeb820e2db85
Heiko-Zelt <hz@heikozelt.de>
2020-10-07 16:07:45 +0200

    added export step

--- import/notices/import_job.sh
+++ import/notices/import_job.sh
@@ -9,12 +9,19 @@ echo
 echo "INFO  Import weg-li-Meldungn"
 cd ../wegli
 ./sync_meldungen.rb
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 echo
 echo "INFO  Aktualisiere Views"
 cd ..
 ./refresh_views.sh
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 echo
 echo "INFO  Zeichne heisse Kacheln neu"
 ./prerender_tiles.sh
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+echo
+echo "INFO  Export Data for Download"
+./export_for_download.sh
