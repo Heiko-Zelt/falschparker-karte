6c731a1f3317e8ec9a4b106875ea0a53d7cb7ed9
Heiko-Zelt <hz@heikozelt.de>
2021-01-23 13:34:44 +0100

    added commentwq

--- import/notices/render_hot_meta_tiles.sh
+++ import/notices/render_hot_meta_tiles.sh
@@ -1,3 +1,4 @@
 # Berechnet, welche Meta Tiles viele Falschparker-Meldungen enthalten
 # und renderd diese Meta Tiles.
+# parallelizing speeds up rendering
 ./calculate_hot_meta_tiles.rb | /usr/bin/render_list -v --num-threads=6 --tile-dir /var/lib/mod_tile/falschparker --min-zoom=0 --force
